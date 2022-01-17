dc70d7ccdc134cbf12d1037caaf28f0c4fcc974c
troy94728 <zlzlskfl13@naver.com>
2022-01-17 16:32:09 +0900

    reset

--- step05_array.js
+++ step05_array.js
@@ -1,7 +1,27 @@
 console.log("1번");
+var max = number[0];
+var min = number[0];
+
+for (var i = 1; i < count; i++) {
+    if (max < number[i]) {
+        max = number[i];
+    } else if (min > number[i]) {
+        min = number[i];
+    }
+}
+
 console.log("");
 
 console.log("2번");
+var max = input[0];
+var maxIdx = 0;
+
+for (var i = 1; i < 9; i++) {
+    if (max < input[i]) {
+        max = input[i];
+        maxIdx = i;
+    }
+}
 console.log("");
 
 console.log("3번");
